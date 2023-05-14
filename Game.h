9f0922b32ed6b986762ac8f0798848a26e3bbb66
Artem-shellkowski <114939650+Artem-shellkowski@users.noreply.github.com>
2023-05-14 23:01:49 +0300

    Add files via upload

---
+++ Game.h
@@ -0,0 +1,121 @@
+﻿#pragma once
+#include "Setings.h"
+#include "playr.h"
+#include "meteor.h"
+#include <vector>
+#include "text.h"
+#include "bonus.h"
+
+class Game {
+private:
+	sf::RenderWindow window;
+	Player player;
+	std::vector<Meteor*> meteorSprites;
+	TextObj lives;
+	sf::RectangleShape rect;
+	std::list<Bonus*> bonusSprites;
+
+	void checkEvents() {
+		sf::Event event;
+		while (window.pollEvent(event))
+			if (event.type == sf::Event::Closed) window.close();
+	}
+
+	void update() {
+		player.update();
+		for (auto& m : meteorSprites) {
+			m->update();
+		}
+		lives.update(std::to_string(player.getLives()));
+		for (auto& bonus : bonusSprites) {
+			bonus->update();
+		}
+	}
+
+
+
+
+	void checkCollisions() {
+		sf::FloatRect playerHitBox = player.getHitBox();
+		auto laserSprites = player.getLasers();
+		for (auto& meteor : meteorSprites) {
+
+			sf::FloatRect meteorHitBox = meteor->getHitBox();
+			if (meteorHitBox.intersects(playerHitBox)) {
+				meteor->spawn();
+				player.receiveDamage(meteor->getDamage());
+			}
+
+			for (auto& laser : (*laserSprites)) {
+				sf::FloatRect laserHitBox = laser->getHitBox();
+
+				if (laserHitBox.intersects(meteorHitBox)) {
+
+					meteor->spawn();
+					laser->setHit();
+					int chance = rand() % BONUS_RANGE;
+					size_t bonusType = rand() % Bonus::BonusType::BONUSES_TYPE_QTY;
+					if (chance < BONUS_CHANCE) {
+						Bonus* bonus = new Bonus(
+							(Bonus::BonusType)bonusType,
+							meteor->getPosition()
+						);
+						bonusSprites.push_back(bonus);
+					}
+				}
+			}
+
+
+			for (auto& bonus : bonusSprites) {
+				sf::FloatRect bonusHitBox = bonus->getHitBox();
+				if (bonusHitBox.intersects(playerHitBox)) {
+					bonus->act(player);
+					bonus->setDel();
+				}
+			}
+		}
+
+		(*laserSprites).remove_if([](Laser* laser) {return laser->isHited(); });
+		(*laserSprites).remove_if([](Laser* laser) {return laser->offScreen(); });
+		bonusSprites.remove_if([](Bonus* bonus) {return bonus->offScreen(); });
+		bonusSprites.remove_if([](Bonus* bonus) {return bonus->isToDel(); });
+	}
+
+	void draw() {
+		window.clear();
+		for (auto m : meteorSprites) {
+			window.draw(m->getSprite());
+		}
+		window.draw(rect);
+		player.draw(window);
+		window.draw(lives.getText());
+		for (auto& bonus : bonusSprites) {
+			bonus->draw(window);
+		}
+		window.display();
+	}
+
+public:
+	Game() :
+		window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE),
+		lives(std::to_string(player.getLives()), sf::Vector2f{ WINDOW_WIDTH / 2,0.f })
+	{
+		window.setFramerateLimit(FPS);
+		meteorSprites.reserve(METEOR_QTY);
+		for (int i = 0; i < METEOR_QTY; i++) {
+			meteorSprites.push_back(new Meteor());
+		}
+		rect.setFillColor(sf::Color::Green);
+		rect.setSize(sf::Vector2f{ WINDOW_WIDTH, 40.f });
+	}
+
+	void play() {
+		while (window.isOpen() && player.isAlive())
+		{
+			checkEvents();
+			update();
+			checkCollisions();
+			draw();
+		}
+	}
+};
---
+++ Setings.h
@@ -0,0 +1,50 @@
+﻿
+#pragma once
+#include "SFML/Graphics.hpp"
+
+const float WINDOW_WIDTH{ 800.f };
+const float WINDOW_HEIGHT{ 800.f };
+const std::string WINDOW_TITLE{ "SFML SPACE SHOOTER " };
+const size_t FPS{ 60 };
+
+const std::string IMAGES_FOLDER{ "images\\" };
+const std::string PLAYER_FILE_NAME{ "playerShip3_orange.png" };
+const sf::Vector2f PLAYER_START_POS{ WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 };
+const int METEORS_QTY{ 20 };
+const sf::Vector2f GAMEOVER_START_POS{ WINDOW_WIDTH * 2, WINDOW_HEIGHT * 2 };
+const std::string GAMEOVER_FILE_NAME{ "gameover.png" };
+
+const std::string LASER_FILE_NAME{ "laserBlue12.png" };
+const float LASER_SPEED = -15.f;
+const int FIRE_COOLDOWN = 200;
+
+const std::string FONT_FILE_NAME = "Samson.ttf";
+const int FONT_SIZE = 32;
+
+
+const std::string MULTI_LASER_BONUS_FILE_NAME{ "things_gold.png" };
+const float BONUS_SPEED{ 15.f };
+
+const std::string HP_BONUS_FILE_NAME{ "pill_yellow.png" };
+const size_t HP_BONUS_MEDICINE = 30;
+const std::string SHIELD_BONUS_FILE_NAME{ "shield_gold.png" };
+const std::string SHIELD_FILE_NAME{ "shield1.png" };
+
+
+const int METEOR_TYPES_QTY = 8;
+const size_t METEOR_QTY = 20;
+
+
+const float PLAYER_SPEED = 10.f;
+const int INITIAL_PLAYER_HP = 100;
+const int FIRE_COOLDOWN = 200;
+
+
+const std::string RES_FOLDER = "res\\";
+
+const int BONUS_RANGE = 10000;
+const int BONUS_CHANCE = 500;
+
+const std::string MULTI_LASER_BONUS_FILE_NAME = "things_gold.png";
+
+const size_t CHAR_SIZE = 32;
